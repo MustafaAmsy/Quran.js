df4e59b4617160384879d1426ac79c58f7b53c02
Amsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-04-01 11:56:02 +0300

    update api.js

--- api.js
+++ api.js
@@ -5,4 +5,4 @@ let d = await fetch('https://api.alquran.cloud/v1/surah/9');
  d = await d.json();
 return d.data;
 }
-(async() => console.log(await ver().name))()
+(async() => console.log(await ver().data.name))()
