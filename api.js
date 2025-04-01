c7f1df291e15ba05634c4e388a67b2cc627e2d7c
Amsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-04-01 11:53:04 +0300

    update api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ async function ver() {
 //const fetch = require('node-fetch');
 let data;
 let d = await fetch('https://api.alquran.cloud/v1/surah/9');
- await d.json();
+ d = await d.json();
 return d.data;
 }
 (async() => console.log(await ver()))()
