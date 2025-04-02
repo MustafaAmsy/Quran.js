b523f1b103a74a1886da150147682105157498cc
Amsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-04-02 12:10:14 +0300

    update api.js

--- api.js
+++ api.js
@@ -5,8 +5,12 @@ async function ver() {
     let response = await fetch('https://api.alquran.cloud/v1/surah/'+i);
     let data = await response.json();
     let d = data.data;
-   let preBasmala = false;
-    dataJson.push({ number: d.number, name: { arabic: d.name, english: { name: d.englishName, translation: d.englishNameTranslation }}, revelation: { arabic: rev[d.revelationType], english: d.revelationType }, versesCount: d.numberOfAyahs, verses: [{ }] })
+   let preBasmala = true;
+   if(i == 9) {
+     preBasmala = false
+   }
+   }
+    dataJson.push({ number: d.number, name: { arabic: d.name, english: { name: d.englishName, translation: d.englishNameTranslation }}, revelation: { arabic: rev[d.revelationType], english: d.revelationType }, versesCount: d.numberOfAyahs, preBasmala,  })
   };
   return dataJson[0];
 }
