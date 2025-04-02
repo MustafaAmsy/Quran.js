a7a534056c1da7a9b1752c2163a3f9620696b60a
Amsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-04-02 16:11:19 +0300

    update api.js

--- api.js
+++ api.js
@@ -3,4 +3,4 @@ const path = require('path');
 const filePath = path.join(__dirname, 'data', 'data.json');
 const fileData = fs.readFileSync(filePath);
 const parsedData = JSON.parse(fileData);
-console.log(parsedData.find(s => s.number === 9))
+console.log(parsedData.find(s => s.number === 1))
