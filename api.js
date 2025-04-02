eb312cd7f90b6ef7c65512e2116c9c08df522d2b
Amsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-04-02 11:49:49 +0300

    update api.js

--- api.js
+++ api.js
@@ -1,9 +1,11 @@
 let dataJson = [];
+let rev = { Meccan: "", Median""
 async function ver() {
   for(let i = 1; 114 >= i;i++) {
     let response = await fetch('https://api.alquran.cloud/v1/surah/'+i);
     let data = await response.json();
-    dataJson.push(data.data)
+    let d = data.data;
+    dataJson.push({ name: { arabic: d.name, english: { name: d.englishName, translation: d.englishNameTranslation }}, revelation: })
   };
   return dataJson[0];
 }
