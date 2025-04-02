6cc7b1997caee9d1220db74a9bfdbb9cc0e6569e
Amsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-04-02 11:45:18 +0300

    update api.js

--- api.js
+++ api.js
@@ -3,7 +3,9 @@ async function ver() {
   for(let i = 1; 114 >= i;i++) {
     let response = await fetch('https://api.alquran.cloud/v1/surah/'+i);
     let data = await response.json();
-  } 
+    dataJson.push(data.data)
+  };
+  return dataJson[0];
 }
 
 (async () => {
