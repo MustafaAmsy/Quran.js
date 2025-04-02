90973546a61e0d8d8472d8e5a63f97153654ce5e
Amsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-04-02 11:43:12 +0300

    update api.js

--- api.js
+++ api.js
@@ -1,10 +1,10 @@
 async function ver() {
     let response = await fetch('https://api.alquran.cloud/v1/surah/9');
     let data = await response.json();
-    return data.data; // data يحتوي على معلومات السورة
+    return data.data.name; // data يحتوي على معلومات السورة
 }
 
 (async () => {
-    let surah = await ver();
-    console.log(surah.name); // استخدام name من كائن السورة
+    
+    console.log(await ver());
 })();
