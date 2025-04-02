2b5d3af819bdcb20f43ee4472ecf705496bf8613
Amsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-04-02 16:14:37 +0300

    update api.js

--- api.js
+++ api.js
@@ -1,24 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const filePath = path.join(__dirname, 'data', 'data.json');
-const rawData = fs.readFileSync(filePath, 'utf-8');
-const surahs = JSON.parse(rawData);
-
-const updatedSurahs = surahs.map(surah => ({
-  ...surah,
-  verses: surah.verses.map(verse => ({
-    ...verse,
-    hizbNumber: Math.floor(verse.hizbQuarter / 4) + 1
-  }))
-}));
-
-// Write back to JSON file
-fs.writeFileSync(filePath, JSON.stringify(updatedSurahs, null, 2), 'utf-8');
-
-console.log("Hizb numbers added successfully!");
-
-
-
-
-
-
+const fileData = fs.readFileSync(filePath);
+const parsedData = JSON.parse(fileData);
+console.log(parsedData.find(s => s.number === 9))
