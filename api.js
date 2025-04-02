601c1318b02379a7254af307eb628edbb2a7574d
Amsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-04-02 16:19:57 +0300

    update api.js

--- api.js
+++ api.js
@@ -1,23 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-
-// Define file path
 const filePath = path.join(__dirname, 'data', 'data.json');
-
-// Read JSON file
-const rawData = fs.readFileSync(filePath, 'utf-8');
-const surahs = JSON.parse(rawData);
-
-// Update each verse with Hizb number, ensuring the number does not exceed 60
-const gSurahs = surahs.map(surah => ({
-  ...surah,
-  verses: surah.verses.map(verse => ({
-    ...verse,
-    hizbNumber: Math.min(Math.floor(verse.hizbQuarter / 4) + 1, 60) // Prevents hizbNumber from going above 60
-  }))
-}));
-
-// Write back to JSON file
-fs.writeFileSync(filePath, JSON.stringify(gSurahs, null, 2), 'utf-8');
-
-console.log("Hizb numbers added successfully!");
+const fileData = fs.readFileSync(filePath);
+const parsedData = JSON.parse(fileData);
+console.log(parsedData.find(s => s.number === 114))
