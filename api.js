3660fa835e046cc32f4cf7aba3ad8884f0898c22
Amsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-04-02 16:43:32 +0300

    update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const filePath = path.join(__dirname, 'data', 'quran.json');
+const filePath = path.join(__dirname, 'data', 'data.json');
 
 // Read JSON file
 const rawData = fs.readFileSync(filePath, 'utf-8');
