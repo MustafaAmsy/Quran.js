b10c4a3e3237813319463ebd025dcabb4ae9c423
Amsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-04-02 15:49:51 +0300

    update api.js

--- api.js
+++ api.js
@@ -13,9 +13,9 @@ async function ver() {
    }
    
    let verses = [];
-   for(let e = 1; d.numberofAyahs >= e; e++) {
+   for(let e = 1; d.numberOfAyahs >= e; e++) {
      const verseData = d.ayahs.find(a => a.numberInSurah === e);
-     verses.push({ text: verseData.text.replaceAll('\n', ''), verseNumber: verseData.number, number: verseData.numberInSurah, juz: verseData.juz, page: verseData.page, hizbQuarter: verseData.hizbQuarter, isSajdah: verseData.sajda})
+     verses.push({ text: verseData.text.replaceAll('\n', ''), verseNumber: verseData.number, number: verseData.numberInSurah, juz: verseData.juz, page: verseData.page, hizbQuarter: verseData.hizbQuarter, isSajdah: verseData.sajda })
    }
     dataJson.push({ number: d.number, name: { arabic: d.name, english: { name: d.englishName, translation: d.englishNameTranslation }}, revelation: { arabic: rev[d.revelationType], english: d.revelationType }, versesCount: d.numberOfAyahs, preBasmala: preBasmala, verses: verses })
   };
