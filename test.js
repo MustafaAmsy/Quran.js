bc250e4fff98f27c41331cd93b2b7b883798ae44
Amsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-04-02 15:44:44 +0300

    update test.js

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
+let doe = []
 async function test() {
 let response = await fetch('https://api.alquran.cloud/v1/surah/1');
     let data = await response.json();
     let d = data.data;
-    return d.ayahs.find(a => a.numberInSurah === 1)
+    for(let i = 1; 7 >= i; i++) {
+      doe[i] = d.ayahs.find(a => a.numberInSurah === 1)
+    }
+    return doe;
 }
 (async() => console.log(await test()))()
